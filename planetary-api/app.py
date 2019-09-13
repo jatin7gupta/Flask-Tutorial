5ed6e1afb84846ea4231be2766d2f733fd815f3b
Jatin Gupta <guptajatin@outlook.com>
2019-09-13 20:55:26 +1000

    add: delete

--- planetary-api/app.py
+++ planetary-api/app.py
@@ -189,6 +189,16 @@ def update_planet():
         return jsonify(message='Planet does not exist'), 404
 
 
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message='You deleted a planet'), 202
+    else:
+        return jsonify(message='Planet not found'), 404
 
 
 # database models
