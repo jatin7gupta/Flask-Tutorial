6013c20000ae2cd888ce1874949b6fb629fe2b83
Jatin Gupta <guptajatin@outlook.com>
2019-09-13 20:17:05 +1000

    Add: post

--- planetary-api/app.py
+++ planetary-api/app.py
@@ -151,6 +151,26 @@ def planet_details(planet_id: int):
         return jsonify(message='planet not found'), 404
 
 
+@app.route('/add_planet', methods=['POST'])
+@jwt_required
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message='There is already a planet by the same name'), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name, planet_type=planet_type, home_star=home_star, mass=mass, radius=radius, distance=distance)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message='You added a planet'), 201
+
+
 # database models
 class User(db.Model):
     __tablename__ = 'users'
